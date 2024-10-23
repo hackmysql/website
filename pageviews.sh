82bd7c497734bf9c19dfde5f218c52c0c47b9fa4
Daniel Nichter <daniel-nichter@users.noreply.github.com>
2024-10-23 16:20:15 -0400

    Update page views

--- pageviews.sh
+++ pageviews.sh
@@ -7,4 +7,5 @@ curl https://api.usefathom.com/v1/aggregations \
   -d "entity_id=WJIYGVMB" \
   -d "aggregates=pageviews" \
   -d "field_grouping=pathname" > out.json
-cat out.json | jq -s '.[] | sort_by(.pageviews|tonumber)'
+#cat out.json | jq -s '.[] | sort_by(.pageviews|tonumber)'
+cat out.json | jq '.[] | .pathname + ":" + .pageviews' | grep -v '/null' | grep -v '/archive' | sed 's/"//g' | sed -e 's#/post##g' | awk -F: '{ p[$1]+=$2} END { for (k in p) print k, p[k] }' | sort -k2 -n
